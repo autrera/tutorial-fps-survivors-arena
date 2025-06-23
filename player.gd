41a7f514db2c9f37f6585ef613263497daa38e60
Aldo Utrera <accounts@aldoutrera.com>
2025-06-23 09:31:35 -0500

    Hide the mouse pointer as default, show on escape press

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * 0.5
@@ -7,3 +11,5 @@ func _unhandled_input(event: InputEvent) -> void:
 		%Camera3D.rotation_degrees.x = clamp(
 			%Camera3D.rotation_degrees.x, -80.0, 80.0
 		)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
