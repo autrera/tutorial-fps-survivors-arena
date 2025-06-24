13ecd19a754977da02b4da3e7b6f3d4d8a229ccc
Aldo Utrera <accounts@aldoutrera.com>
2025-06-24 11:47:29 -0500

    Code the ground movement

--- player.gd
+++ player.gd
@@ -13,3 +13,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		)
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta: float) -> void:
+	const SPEED = 5.5
+	
+	var input_direction_2D = Input.get_vector(
+		"move_left", "move_right", "move_forward", "move_back"
+	)
+	var input_direction_3D = Vector3(
+		input_direction_2D.x, 0.0, input_direction_2D.y
+	)
+	var direction = transform.basis * input_direction_3D
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	
+	move_and_slide()
