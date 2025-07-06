500c78ad3e6b38d6f57258735dfeaf8810d38ba3
Aldo Utrera <accounts@aldoutrera.com>
2025-07-06 09:35:16 -0500

    Add taking damage and ko sounds to the mob

--- mob/mob.gd
+++ mob/mob.gd
@@ -8,6 +8,8 @@ var speed = randf_range(2.0, 4.0)
 @onready var bat_model: Node3D = %bat_model
 @onready var player = get_node("/root/Game/Player")
 @onready var timer: Timer = %Timer
+@onready var hurt_sound: AudioStreamPlayer3D = %HurtSound
+@onready var ko_sound: AudioStreamPlayer3D = %KOSound
 
 
 func _physics_process(delta: float) -> void:
@@ -21,6 +23,7 @@ func take_damage():
 		return
 	
 	bat_model.hurt()
+	hurt_sound.play()
 	health -= 1
 	
 	if health == 0:
@@ -32,6 +35,7 @@ func take_damage():
 		apply_central_impulse(direction * 10.0 + random_upward_force)
 		timer.start()
 		died.emit()
+		ko_sound.play()
 
 
 func _on_timer_timeout() -> void:
